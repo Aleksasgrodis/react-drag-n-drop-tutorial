4a799d5487007ab6f6b38c84595e8bca845d07a4
Aleksas Grodis <aleksgrodis@hotmail.com>
2020-09-08 16:57:24 +0200

    Finished React-DnD chess tutorial.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Board from './components/Board';
+
+function App() {
+  return <Board knightPosition={[4, 5]} />;
+}
+
+export default App;
---
+++ src/components/Knight.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ItemTypes } from '../constants/Constants';
+import { useDrag } from 'react-dnd';
+
+function Knight() {
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: ItemTypes.KNIGHT },
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+  return (
+    <div
+      ref={drag}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        cursor: 'move',
+      }}
+    >
+      ♘
+    </div>
+  );
+}
+
+export default Knight;
